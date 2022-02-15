e504f8f2477404c9ea06987943196b08333539da
miftah1991 <miftah.amin1991@gmail.com>
2022-02-15 22:38:48 +0430

    Setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+    }),
+  ],
+};
