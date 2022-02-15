fdc47072d10f5d572c95f2807e8f40b2ca1f8ff8
miftah1991 <miftah.amin1991@gmail.com>
2022-02-16 00:10:48 +0430

    Add Todo list functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,45 @@
-import _ from 'lodash';
-
+// imnpport _ from 'lodash';
+import './style.css';
+const tasks = [
+    {
+      description: 'Listing item 1',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'Completed todo list item',
+      completed: false,
+      index: 7,
+    },
+    {
+      description: 'Drag and drop time',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'Remove all completed items',
+      completed: false,
+      index: 3,
+    },
+    {
+      description: 'items should be in sorted order',
+      completed: false,
+      index: 4,
+    },
+  ];
+  const tasksDisplay = () => {
+      const inputItem = document.querySelector('#input-item')
+    tasks.reverse().forEach((task) => {
+      let html = `
+      <li class="task-${task.index}">
+        <div class="task">
+          <input type="checkbox" name="task"> ${task.description}
+        </div>
+        <i class="fas fa-ellipsis-v three-dot-icon"></i>
+      </li>
+      `;
+      inputItem.insertAdjacentHTML("afterend", html);
+    });
+  };
+  tasksDisplay();
+  
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Todo List',
+      template: "./src/index.html",
+      filename: "index.html",
     }),
   ],
 };
